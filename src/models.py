26062b50567efc005e61532018786a4a05f56ce8
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-05-21 02:13:23 +0000

    Se hizo relaciones de tablas

--- src/models.py
+++ src/models.py
@@ -7,26 +7,48 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(50), unique=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(50), unique=True)
+    password = Column(String(32))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_from_id_relationship = relationship(User)
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id_relationship = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comment_text = Column(String(100))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    author_id_relationship = relationship(User)
+    post_id = Column(Integer)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, ForeignKey('comment.post_id'), primary_key=True)
+    id_relationship = relationship(Comment)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id_relationship = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String) #aca debe ir dato del tipo enum pero da error, preguntar.
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post_id_relationship = relationship(Post)
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
